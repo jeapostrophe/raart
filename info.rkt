0c09efe7fbda1c5448c4319095965e3ea1650b3e
Jay McCarthy <jay.mccarthy@gmail.com>
2018-01-01 19:23:32 -0500

    Split

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "raart")
-(define deps '("ansi"
+(define deps '("reprovide-lang"
+               "ansi"
                "base"))
 (define build-deps '())
 (define version "0.1")
