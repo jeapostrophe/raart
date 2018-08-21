1a41aacff21afdeae6e138805194e808ca7fac53
Jay McCarthy <jay.mccarthy@gmail.com>
2018-08-21 16:29:21 -0400

    Deps

--- info.rkt
+++ info.rkt
@@ -6,7 +6,10 @@
                "ansi"
                "struct-define"
                "base"))
-(define build-deps '())
+(define build-deps '("htdp-doc"
+                     "racket-doc"
+                     "scribble-lib"
+                     ))
 (define version "0.1")
 (define pkg-authors '(jeapostrophe))
 (define scribblings '(("raart.scrbl" () ("UI"))))
